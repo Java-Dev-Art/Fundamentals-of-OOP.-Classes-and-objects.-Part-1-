ae5ac07cb25f92adc3b0caa2f59dc22a0eed9c0f
Artur Markouski <veles.mk.12@gmail.com>
2023-02-08 11:49:39 +0100

    feat: creted test2 for solving task

--- src/main/java/Test2.java
+++ src/main/java/Test2.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Test2 {
+public class Test2 {
+    private int valueFirst;
+    private int valueSecond;
+
+    public Test2(int valueFirst, int valueSecond) {
+        this.valueFirst = valueFirst;
+        this.valueSecond = valueSecond;
+    }
+
+    public Test2() {
+        valueFirst = 0;
+        valueSecond = 1;
+    }
+
+    public int getValueFirst() {
+        return valueFirst;
+    }
+
+    public void setValueFirst(int valueFirst) {
+        this.valueFirst = valueFirst;
+    }
+
+    public int getValueSecond() {
+        return valueSecond;
+    }
+
+    public void setValueSecond(int valueSecond) {
+        this.valueSecond = valueSecond;
+    }
 }
